79f5218721ac4ab94d16f22182b1f6440fe2ecbc
itd123 <sandrex.kami@gmail.com>
2019-10-07 18:15:09 +0800

    testing

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once('report.php');
 
 $class = new Report();
 
-// $data = $class->get(); //1st
+$data = $class->get(); //1st
 // $data = $class->PR_BORROWERS(); //second
 // $data = $class->TRANSMASTER(); //third
 // $data = $class->getUpdate(); //fourth
